9550c9568ef5acfb40995b98dd6df4127096fbb6
Dafreeman794 <nategcook@gmail.com>
2026-04-21 10:22:23 -0700

    experimented with our arrow feature.

--- rooms.js
+++ rooms.js
@@ -1,7 +1,7 @@
 //Defining our arrays
 const imageslevel0 = [
     {image: 'images/level0.png', weight: 1},
-    {image: 'images/Level_0_alt 1.png', weight: 10},
+    {image: 'images/door.png', weight: 30},
     {image: 'images/arrows.png', weight: 10},
     {image: 'images/dark 1.png', weight: 10},
     {image: 'images/iconic.png', weight: 10},
@@ -9,7 +9,7 @@ const imageslevel0 = [
     {image: 'images/THEimage.png', weight: 10},
     {image: 'images/vhs.png', weight: 10},
     {image: 'images/hall 1.png', weight: 10},
-    {image: 'images/chair1.png', weight: 10},
+    {image: 'images/chairs.png', weight: 10},
     {image: 'images/noshelves.png', weight: 10}
 ];
 
@@ -25,18 +25,33 @@ const imageslevel37 = [
     {image: 'images/winding.png', weight: 10}
 ];
 
+const imageslevel5 = [
+    {image: 'images/poolhall.png', weight: 10},
+    {image: 'images/pools 1.png', weight: 10},
+    {image: 'images/stairs.png', weight: 10},
+    {image: 'images/winding.png', weight: 10}
+]
+
 const imageslevel94 = [
     {image: 'images/house clear view.png', weight: 10},
     {image: 'images/insidehouse(1).png', weight: 10},
     {image: 'images/stop.png', weight: 10}
 ];
 
+
+const imageslevellimbo = [
+    {image: 'images/youcheated.png', weight: 10},
+    {image: 'images/limbostair.png', weight: 10}
+];
+
 //all documents used
 const imageArrays = {
     'Level 0': imageslevel0,
     'Level 1': imageslevel1,
+    'level 5': imageslevel5,
     'Level 37': imageslevel37,
-    'Level 94': imageslevel94
+    'Level 94': imageslevel94,
+    'limbo': imageslevellimbo
 };
 
 let lastImage = null;
@@ -76,9 +91,9 @@ function changeImage() {
         imgElement.src = selected.image;
 
 // protopyte Level exit logic (only works for level 0 door at the moment)
-        if (selected.image === 'images/level0.png') {
+        if (selected.image === 'images/door.png') {
             imgElement.style.cursor = 'pointer';
-            imgElement.onclick = () => window.location.href = 'level1.html';
+            imgElement.onclick = (arrow) => window.location.href = 'level1.html';
         } else {
             imgElement.style.cursor = 'default';
             imgElement.onclick = null; 
@@ -92,3 +107,7 @@ window.changeImage = changeImage;
 document.addEventListener('DOMContentLoaded', () => {
     changeImage();
 });
+
+function playoption () {
+    
+}
